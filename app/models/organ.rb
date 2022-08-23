603711a8c52c1dc6d693a5861f8e05adee611af7
Alexis Gonzalez <alexis_gonzalez@hotmail.fr>
2022-08-23 12:34:49 +0200

    add_validations

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -2,4 +2,11 @@ class Organ < ApplicationRecord
   belongs_to :user
   has_one :review
   has_one :booking
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :availability, presence: true
+  validates :price, presence: true
 end
