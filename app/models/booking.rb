38c755e537f7e6f7dd215e4aef0b7116f66eb500
Alexis Gonzalez <alexis_gonzalez@hotmail.fr>
2022-08-25 16:03:40 +0200

    add method update booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :organ
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :availability, presence: true
   enum availability: { free: 0, pending: 1, sold: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_101900) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_134454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "organ_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "availability"
+    t.integer "availability", default: 0
     t.index ["organ_id"], name: "index_bookings_on_organ_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
