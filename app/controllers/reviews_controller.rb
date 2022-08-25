a135f2ad503eb843349bf843b915f0c8ec7f7a14
sherazade othman <othmansherazade@gmail.com>
2022-08-25 13:54:25 +0200

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,13 @@
 class ReviewsController < ApplicationController
-  def new
-  end
-
   def create
+    @organ = Organ.find(params[:organ_id])
+    @review = Review.new(review_params)
+    @review.organ = @organ
+    if @review.save
+      redirect_to dashboard_path
+    else
+      # render :dashboard, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -12,5 +17,13 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
 
   def dashboard
     @bookings = Booking.where(user: current_user)
+    @review = Review.new
   end
 end
