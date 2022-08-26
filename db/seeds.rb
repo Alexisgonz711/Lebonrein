a9c1e605653c4cd6fed0583c2571a6c773c28eb3
sherazade othman <othmansherazade@gmail.com>
2022-08-26 16:38:43 +0200

    images index et show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require 'open-uri'
 
 Booking.destroy_all
 Organ.destroy_all
@@ -41,12 +42,75 @@ puts "#{User.count} users created!"
 puts "Destroying organs..."
 puts "Creating organs..."
 
-jeune = Organ.create!(name: "Coeur d'un jeune", category: "Coeur", pricing: 22000,  description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500", image_url: "blablalblalbalblbalba", user: sherazade )
-poumon = Organ.create!(name: "Je vends mon poumon", category: "poumon", description: "neuf", user: elea, pricing: 10000)
-coeur = Organ.create!(name: "Je vends un coeur trouvé dans une poubelle", category: "coeur", description: "un peu amoché", user: alexis, pricing: 20000)
+jeune = Organ.create!(name: "Coeur d'un jeune", category: "Coeur", pricing: 22000,  description: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500", user: sherazade )
 
-categories = ["poumon", "coeur", "rein", "foie", "rate", "estomac", "intestin", "penis", "clitoris", "vessie", "pancréas", "apendice", "vésicule billiaire"]
+categories = ["poumon", "coeur", "rein", "foie", "rate", "estomac", "intestin", "penis", "clitoris", "vessie", "pancréas", "appendice", "vésicule billiaire"]
 new_organs = []
+
+file = URI.open("https://st3.depositphotos.com/6563466/32179/i/450/depositphotos_321794782-stock-photo-human-body-organs-lungs-anatomy.jpg")
+poumon = Organ.new(name: "Je vends mon poumon", category: "Poumon", description: "Poumon neuf prêt à l'emploi", user: elea, pricing: 10000)
+poumon.image_url.attach(io: file, filename: "poumon.jpg", content_type: "image/jpg")
+poumon.save
+
+file = URI.open("https://st.depositphotos.com/1772771/3829/i/450/depositphotos_38293543-stock-photo-heart.jpg")
+coeur = Organ.new(name: "Je vends un coeur trouvé dans une poubelle", category: "Coeur", description: "Un peu amoché", user: alexis, pricing: 20000)
+coeur.image_url.attach(io: file, filename: "coeur.jpg", content_type: "image/jpg")
+coeur.save
+
+file = URI.open("https://st4.depositphotos.com/1001877/26726/i/450/depositphotos_267265320-stock-photo-large-and-small-intestineisolated-on.jpg")
+intestin = Organ.new(name: "Intestin à vendre!", category: "Intestin", description: "Intestin en bon état", user: elea, pricing: 30000)
+intestin.image_url.attach(io: file, filename: "intestin.jpg", content_type: "image/jpg")
+intestin.save
+
+file = URI.open("https://st2.depositphotos.com/1987851/7369/i/450/depositphotos_73695083-stock-photo-realistic-human-liver-illustration.jpg")
+foie = Organ.new(name: "Foie d'un vieux", category: "Foie", description: "Foie bien usé mais en bon état", user: alexis, pricing: 15000)
+foie.image_url.attach(io: file, filename: "intestin.jpg", content_type: "image/jpg")
+foie.save
+
+file = URI.open("https://st4.depositphotos.com/6563466/30996/i/450/depositphotos_309969042-stock-photo-human-kidneys-digital-illustration-black.jpg")
+rein = Organ.new(name: "Reins à vendre!", category: "Reins", description: "Reins en bon état", user: sherazade, pricing: 25000 )
+rein.image_url.attach(io: file, filename: "rein.jpg", content_type: "image/jpg")
+rein.save
+
+file = URI.open("https://us.123rf.com/450wm/madrock24/madrock242005/madrock24200500052/146832507-anatomie-de-la-rate-humaine-organes-isol%C3%A9s-sur-fond-blanc.jpg?ver=6")
+rate = Organ.new(name: "Rate à vendre!", category: "Rate", description: "Rate en très bon état!", user: elea, pricing: 30000)
+rate.image_url.attach(io: file, filename: "rate.jpg", content_type: "image/jpg")
+rate.save
+
+file = URI.open("https://image.shutterstock.com/image-illustration/3d-human-stomach-anatomy-detail-260nw-1440375524.jpg")
+estomac = Organ.new(name: "Estomac à vendre!", category: "Estomac", description: "Estomac en assez bon état", user: sherazade, pricing: 15000)
+estomac.image_url.attach(io: file, filename: "estomac.jpg", content_type: "image/jpg")
+estomac.save
+
+file = URI.open("https://st4.depositphotos.com/6563466/30996/i/450/depositphotos_309969046-stock-photo-human-bladder-digital-illustration-black.jpg")
+vessie = Organ.new(name: "Vessie à vendre!", category: "Vessie", description: "Vessie en super état", user: elea, pricing: 35000)
+vessie.image_url.attach(io: file, filename: "vessie.jpg", content_type: "image/jpg")
+vessie.save
+
+file = URI.open("https://as2.ftcdn.net/v2/jpg/01/80/94/67/1000_F_180946759_wXMUKcB80dtGwQcI7eie4wW5lOLV8bOu.jpg")
+pancreas = Organ.new(name: "Pancréas à vendre!", category: "Pancréas", description: "Pancréas pas top", user: alexis, pricing: 15000)
+pancreas.image_url.attach(io: file, filename: "pancreas.jpg", content_type: "image/jpg")
+pancreas.save
+
+file = URI.open("https://static.lexpress.fr/medias_12339/w_1621,h_1216,c_crop,x_204,y_67/w_640,h_358,c_fill,g_center/v1628060010/appendix-pain-illustration_6317644.jpg")
+appendice = Organ.new(name: "Appendice à vendre!", category: "Appendice", description: "Appendice en bon état", user: elea, pricing: 25000)
+appendice.image_url.attach(io: file, filename: "appendice.jpg", content_type: "image/jpg")
+appendice.save
+
+file = URI.open("https://st.depositphotos.com/1229718/4883/i/450/depositphotos_48830849-stock-photo-gallbadder.jpg")
+vesicule = Organ.new(name: "Vésicule billiaire à vendre!", category: "Vésicule billiaire", description: "Vésicule billiaire en bon état", user: elea, pricing: 15000)
+vesicule.image_url.attach(io: file, filename: "vesicule.jpg", content_type: "image/jpg")
+vesicule.save
+
+file = URI.open("https://www.francetvinfo.fr/pictures/THtafqrFy5aqIY0-UapFcuUN2ss/fit-in/720x/2017/09/01/phplHG2DL_1.jpg")
+clitoris = Organ.new(name: "Clitoris à vendre!", category: "Clitoris", description: "Clitoris en très bon état", user: alexis, pricing: 50000)
+clitoris.image_url.attach(io: file, filename: "clitoris.jpg", content_type: "image/jpg")
+clitoris.save
+
+file = URI.open("https://i0.wp.com/ae01.alicdn.com/kf/H0eac5c2003644bfda18df593b88cff09V.jpg?fit=510%2C510&ssl=1")
+penis = Organ.new(name: "Penis à vendre!", category: "Penis", description: "Penis en super état!!", user: sherazade, pricing: 50000)
+penis.image_url.attach(io: file, filename: "penis.jpg", content_type: "image/jpg")
+penis.save
 
 57.times do
   new_organs << categories.sample
